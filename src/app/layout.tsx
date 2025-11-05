0902a0f65a0c72ae9db417e100ec0db30d54a03d
Sarschi03 <165720330+Sarschi03@users.noreply.github.com>
2025-11-05 12:17:57 +0100

    logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,26 +17,20 @@ export const metadata: Metadata = {
   description: "Global Trade Solution Provider",
   icons: {
     icon: [
+      { url: "/ms.logo1.png", type: "image/png", sizes: "16x16" },
       { url: "/ms.logo1.png", type: "image/png", sizes: "32x32" },
-      { url: "/ms.logo1.png", type: "image/png", sizes: "192x192" }, // optional larger
-      { url: "/favicon.ico", sizes: "any" }, // optional ICO fallback
     ],
-    apple: "/ms.logo2.png",
-    shortcut: "/ms.logo2.png",
+    shortcut: "/ms.logo3.png",
+    apple: "/ms.logo3.png",
   },
   openGraph: {
     title: "Macau Station",
     description: "Global Trade Solution Provider",
     url: "https://www.macaustation.com",
     siteName: "Macau Station",
-    images: [{ url: "/og-image.png", width: 1200, height: 630 }], // optional
+    
   },
-  twitter: {
-    card: "summary_large_image",
-    title: "Macau Station",
-    description: "Global Trade Solution Provider",
-    images: ["/og-image.png"], // optional
-  },
+  
 };
 
 export default function RootLayout({
